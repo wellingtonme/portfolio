08d890fd7f9363ee40870856e9f88536bd67f9c3
Wellington Messias da Silva <wellington.messias@inatel.br>
2018-02-08 12:40:40 -0200

    create presentation card

---
+++ src/components/pages/presentation/CardTextRow.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-layout
+    row
+    wrap
+  >
+    <v-flex>
+      <div :class="classes">
+        <slot/>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+	name: 'CardTextRow',
+  props: {
+    classes: {
+      type: String,
+      default: ('')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/pages/presentation/Presentation.vue
+++ src/components/pages/presentation/Presentation.vue
@@ -1,49 +1,57 @@
 <template>
   <section>
-    <v-parallax>
-
-      <v-container 
-        fluid
-        class="presentation-card">
-        <v-layout
-          row
-          wrap>
-          <v-flex 
-            xs12
-            sm12
-            md8
-            lg6
-            xl6
-            offset-md2
-            offset-lg3>
-            <v-card>
-              <v-card-text>
-                <v-container>
-                  <v-layout
-                    row
-                    wrap
-                  >
-                    <v-flex>
-                      <div class="profile-preword elevation-1">
-                        <span>Hi there!</span>
-                      </div>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card-text>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-
-    </v-parallax>
+    <v-parallax height="400"/>
+    <v-container 
+      fluid
+      class="presentation-card">
+      <v-layout
+        row
+        wrap>
+        <v-flex 
+          xs12
+          sm12
+          md8
+          lg6
+          xl6
+          offset-md2
+          offset-lg3>
+          <v-card>
+            <v-card-text>
+              <v-container>
+                <card-text-row classes="profile--preword">
+                  <span>Hello there!</span>
+                </card-text-row>
+                <card-text-row classes="profile--name">
+                  <span>I'm Wellington,</span>
+                </card-text-row>
+                <card-text-row classes="profile--role">
+                  <span>Fullstack developer &lt; &frasl; &gt;</span>
+                </card-text-row>
+                <card-text-row classes="profile--description">
+                  <p>I'm a Web Fullstack Developer who loves VueJs and Node, 
+                  passionate problem-solver, owner of 4 cats &#128062; and 
+                  I like to watch videos of dressed dancing cats.</p>
+                </card-text-row>
+                <card-text-row classes="profile--quote">
+                  <span>Working to make the internet a better place!</span>
+                </card-text-row>
+              </v-container>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </section>
 </template>
 
 <script>
+import CardTextRow from "./CardTextRow.vue";
 export default {
-	name: 'Presentation'
-}
+  name: "Presentation",
+  components: {
+    CardTextRow
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
@@ -55,8 +63,8 @@ export default {
 	background-image url('https://c2.staticflickr.com/8/7331/9262740777_f69bca112c_b.jpg')
 	background-size cover
 	background-position-y center
-	&:before 
-		background-color rgba(0,0,0,0.8)
+	&::before
+		background-color rgba(0, 0, 0, 0.8)
 		content ''
 		top 0
 		left 0
@@ -65,23 +73,32 @@ export default {
 		position absolute
 
 .presentation-card
-	margin-top: 150px
+	margin-top 200px
 
-.profile-preword
-	float left
-	background-color #2196F3
-	padding 10px
-	color: #030303
-	& span::before
-		content ''
-		width 0
-		height 0
-		top 70%
-		left 32px
-		display block 
-		position absolute
-		border-style solid
-		border-width 0 0 8px 8px
-		border-color transparent
-		border-left-color #2196F3
+.profile
+	&--preword
+		& > span
+			font-size 12px
+			color #bebebe
+	&--name
+		& > span
+			font-size 40px
+			font-weight lighter
+	&--role
+		& > span
+			font-size 30px
+			font-weight bold
+			color #bebebe
+	&--description
+		& > p
+			padding-top: 10px !important
+			font-size 16px
+			font-weight lighter
+			color #bebebe
+			margin-bottom 10px
+	&--quote
+		& > span
+			font-size 16px
+			font-style italic
+			color #bebebe
 </style>
